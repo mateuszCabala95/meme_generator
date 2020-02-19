146fe58aa253ea5eeda9e64dd215d0121fd272d2
Mateusz <mateusz.cabala95@gmail.com>
2020-02-19 22:51:24 +0100

    Updates

---
+++ precache-manifest.75362c1b96f1af16dafb246399cec29d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610cdbdfe369a1fa2a3b63924c70665a",
+    "url": "/meme_generator/index.html"
+  },
+  {
+    "revision": "16b722d1f1bbae26d834",
+    "url": "/meme_generator/static/css/main.d9e691d4.chunk.css"
+  },
+  {
+    "revision": "d39e6d7bacdcc38b9fe7",
+    "url": "/meme_generator/static/js/2.34e26a7b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/meme_generator/static/js/2.34e26a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b722d1f1bbae26d834",
+    "url": "/meme_generator/static/js/main.d062745b.chunk.js"
+  },
+  {
+    "revision": "2b668981bb977292c5c5",
+    "url": "/meme_generator/static/js/runtime-main.90a75d4d.js"
+  }
+]);
